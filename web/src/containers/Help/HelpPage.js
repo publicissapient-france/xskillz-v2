54cc83dae86a2cefac7b19b01f2974378b93fe2e
jsmadja <julien.smadja@gmail.com>
2016-07-24 16:16:54 +0200

    Help : Style

--- web/src/containers/Help/HelpPage.js
+++ web/src/containers/Help/HelpPage.js
@@ -1,28 +1,36 @@
 import React, {Component} from "react";
 import {redA400} from "material-ui/styles/colors";
 import Stars from "../../components/Skills/Stars";
+import Paper from 'material-ui/Paper';
+
+const style = {
+    margin: 20,
+    padding: 10
+};
 
 export class HelpPage extends Component {
     render() {
         return (
             <div id="main">
                 <div className="content">
-                    <h2>Aide</h2>
+                    <Paper style={style}>
+                        <h4>Pourquoi certaines cartes de compétences sont quasi transparentes ?</h4>
+                        <p>
+                            Une carte de compétence s'éclaircie lorsqu'elle ne possède pas le petit <span className="interested-icon" style={{color: redA400}}>&#9829;</span>. Ainsi, les compétences qui
+                            intéressent le profil consulté sont mises en avant.
+                        </p>
+                    </Paper>
 
-                    <h3>Pourquoi certaines cartes de compétences sont quasi transparentes ?</h3>
-                    <p>
-                        Une carte de compétence s'éclaircie lorsqu'elle ne possède pas le petit <span
-                        className="interested-icon" style={{color: redA400}}>&#9829;</span>. Ainsi, les compétences qui
-                        intéressent le profil consulté sont mises en avant.
-                    </p>
-
-                    <h3>Que représentent les <Stars level={1}/> ?</h3>
+                    <Paper style={style}>
+                        <h4>Que représentent les <Stars level={1}/> ?</h4>
                     <p>
                         Pour chaque compétence, l'utilisateur est invité à estimer son niveau de connaissance. Chaque
                         entreprise possède sa propre échelle de valeur.
                         Nous pensons qu'une échelle de 0 à 3 est suffisante.
                     </p>
-                    Voici l'échelle que nous avons définie chez Xebia :
+                    <p>
+                        Voici l'échelle que nous avons définie chez Xebia :
+                    </p>
                     <table>
                         <tbody>
                         <tr>
@@ -67,8 +75,15 @@ export class HelpPage extends Component {
                         </tr>
                         </tbody>
                     </table>
+                    </Paper>
 
-                    <h3>Que faire si j'ai oublié mon mot de passe ?</h3>
+                    <Paper style={style}>
+                    <h4>Que faire si j'ai oublié mon mot de passe ?</h4>
+                    </Paper>
+
+                    <Paper style={style}>
+                        <h4>J'ai trouvé un bug, j'ai une idée, j'ai une question, qui puis-je contacter ?</h4>
+                    </Paper>
                 </div>
             </div>
         );
