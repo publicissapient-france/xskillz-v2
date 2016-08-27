156484dd42e45bd381c53cc3d3e3d31774a25494
jsmadja <julien.smadja@gmail.com>
2016-08-27 17:23:34 +0200

    Log JSON response

--- back/src/index.js
+++ back/src/index.js
@@ -3,6 +3,12 @@
 const Express = require('express');
 const Database = require('./database');
 
+var winston = require('winston'),
+    expressWinston = require('express-winston');
+
+//expressWinston.requestWhitelist.push('body');
+expressWinston.responseWhitelist.push('body');
+
 const UserController = require('./user/user-controller');
 const SkillController = require('./skill/skill-controller');
 const DomainController = require('./domain/domain-controller');
@@ -22,7 +28,21 @@ Express()
     .use(allowCrossDomain)
     .use(require('body-parser').json())
     .use(require('cors')())
-
+    .use(expressWinston.logger({
+        transports: [
+            new winston.transports.Console({
+                json: true,
+                colorize: true
+            })
+        ],
+        meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+        msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+        expressFormat: true, // Use the default Express/morgan request formatting, with the same colors. Enabling this will override any msg and colorStatus if true. Will only output colors on transports with colorize set to true
+        colorStatus: true, // Color the status code, using the Express/morgan color palette (default green, 3XX cyan, 4XX yellow, 5XX red). Will not be recognized if expressFormat is true
+        ignoreRoute: function (req, res) {
+            return false;
+        } // optional: allows to skip some log messages based on request and/or response
+    }))
     .use(function (req, res, next) {
         const token = req.headers.token;
         if (token) {
@@ -57,6 +77,7 @@ Express()
     .get('/users/:id', UserController.getUserById)
     .put('/users/:id', UserController.updateUser)
     .post('/users/:id/manager/:managerId', UserController.assignManager)
+    .post('/users/:id/manager', UserController.promoteToManager)
     .delete('/users/:id', UserController.deleteUserById)
     .post('/signin', UserController.signin)
 
