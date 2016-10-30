fe40075b1233de93e458a1773ba5c7ac86d5c8dc
jsmadja <julien.smadja@gmail.com>
2016-10-30 13:19:06 +0100

    Remove express-winston Change DB connection

--- back/src/index.js
+++ back/src/index.js
@@ -3,12 +3,6 @@
 const Express = require('express');
 const Database = require('./database');
 
-var winston = require('winston'),
-    expressWinston = require('express-winston');
-
-//expressWinston.requestWhitelist.push('body');
-//expressWinston.responseWhitelist.push('body');
-
 const UserController = require('./user/user-controller');
 const SkillController = require('./skill/skill-controller');
 const DomainController = require('./domain/domain-controller');
@@ -28,21 +22,6 @@ Express()
     .use(allowCrossDomain)
     .use(require('body-parser').json())
     .use(require('cors')())
-    .use(expressWinston.logger({
-        transports: [
-            new winston.transports.Console({
-                json: true,
-                colorize: true
-            })
-        ],
-        meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-        msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
-        expressFormat: true, // Use the default Express/morgan request formatting, with the same colors. Enabling this will override any msg and colorStatus if true. Will only output colors on transports with colorize set to true
-        colorStatus: true, // Color the status code, using the Express/morgan color palette (default green, 3XX cyan, 4XX yellow, 5XX red). Will not be recognized if expressFormat is true
-        ignoreRoute: function (req, res) {
-            return false;
-        } // optional: allows to skip some log messages based on request and/or response
-    }))
     .use(function (req, res, next) {
         const token = req.headers.token;
         if (token) {
