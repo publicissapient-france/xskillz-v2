d3451e55ea3ac1baf6eb3cdaca68ba421a1c3190
Benjamin LACROIX <benjlacroix@gmail.com>
2016-11-20 13:42:38 +0100

    #40 add roles to dn

---
+++ back/database/migrations/sqls/20161120123511-roles-down.sql
@@ -0,0 +1,8 @@
+DELETE FROM Role
+WHERE name = 'skills';
+DELETE FROM Role
+WHERE name = 'users';
+DELETE FROM Role
+WHERE name = 'management';
+DELETE FROM Role
+WHERE name = 'settings';
---
+++ back/database/migrations/sqls/20161120123511-roles-up.sql
@@ -0,0 +1,4 @@
+INSERT INTO Role (name) VALUES ('skills');
+INSERT INTO Role (name) VALUES ('users');
+INSERT INTO Role (name) VALUES ('management');
+INSERT INTO Role (name) VALUES ('settings');
