fc51a5e66290b0fa5dfea18e01e69c7634c85fd8
jsmadja <julien.smadja@gmail.com>
2017-09-11 09:19:27 +0200

    Label Pas de manageur assigné

--- web/src/components/Management/ManagerItem.js
+++ web/src/components/Management/ManagerItem.js
@@ -7,7 +7,7 @@ class ManagerItem extends Component {
         return (
             <Paper>
                 <div className="manager-container">
-                    <span className="manager" onClick={()=>onUserClick(manager.manager.readable_id)}>{manager.manager.name} ({manager.users.length})</span>
+                    <span className="manager" onClick={()=>onUserClick(manager.manager.readable_id)}>{manager.manager.name || 'Pas de manageur assigné'} ({manager.users.length})</span>
                     {
                         manager.users.map((user, index) => (
                             <p className="user" key={index} onClick={()=>onUserClick(user.readable_id)}>{user.name}</p>)
