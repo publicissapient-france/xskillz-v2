4841053fd02ad9252667545d04c08ee9b67aa79e
jsmadja <julien.smadja@gmail.com>
2017-09-11 09:16:14 +0200

    Add Role Card in migration

--- back/database/migrations/sqls/20161120123511-roles-up.sql
+++ back/database/migrations/sqls/20161120123511-roles-up.sql
@@ -2,3 +2,4 @@ INSERT INTO Role (name) VALUES ('skills');
 INSERT INTO Role (name) VALUES ('users');
 INSERT INTO Role (name) VALUES ('management');
 INSERT INTO Role (name) VALUES ('settings');
+INSERT INTO Role (name) VALUES ('card');
